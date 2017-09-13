bca578aa70b882bec2fb583a28416d77c59df5ec
Gordon Tisher <gordon@balafon.net>
2017-09-13 05:43:55 -0700

    Update Windows build to use 'build/release' and 'build/debug' directories (#36)
    * Update Windows build to use 'build/release' and 'build/debug' directories.
    
    Changed the `make.bat` file to allow the following:
    
    ```
    make                # builds release in build/release
    make config=release # builds release in build/release
    make config=debug   # builds debug in build/debug
    make clean          # deletes build/release
    make clean config=release # deletes build/release
    make clean config=debug   # deletes build/debug
    ```

--- make.bat
+++ make.bat
@@ -1,25 +1,43 @@
 @echo off
+
 if "%1"=="--help" goto help
-if "%1"=="clean" goto clean
+if "%1"=="help" goto help
+
+set GOTOCLEAN=false
+if "%1"=="clean" (
+  set GOTOCLEAN=true
+  shift
+)
+
+set CONFIG=release
+set DEBUG=
+if "%1"=="config" (
+  if "%2"=="debug" (
+    set CONFIG=debug
+    set DEBUG=--debug
+  )
+)
+
+set BUILDDIR=build\%CONFIG%
+if "%GOTOCLEAN%"=="true" goto clean
 
 where ponyc > nul
 if errorlevel 1 goto noponyc
 
 :build
-set DEBUG=
-if "%1"=="--debug" set DEBUG="--debug"
-if not exist bin mkdir bin
-echo Compiling stable...
-ponyc stable -o bin %DEBUG%
+if not exist "%BUILDDIR%" mkdir "%BUILDDIR%""
+echo Compiling: ponyc stable -o %BUILDDIR% %DEBUG%
+ponyc stable -o %BUILDDIR% %DEBUG%
 goto done
 
 :clean
-echo Removing ./bin...
-rmdir /s /q bin
+if not exist "%BUILDDIR%" goto done
+echo Removing "%BUILDDIR%"
+rmdir /s /q "%BUILDDIR%"
 goto done
 
 :help
-echo usage: make.bat [clean ^| --debug]
+echo usage: make.bat [clean] [config=release|debug]
 goto done
 
 :noponyc
