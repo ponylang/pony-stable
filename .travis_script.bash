bae8cb54d789fdbf7943d6c99e65237e7dc7a64c
Sean T Allen <sean@monkeysnatchbanana.com>
2017-09-16 18:14:16 -0400

    Update to latest changelog-tool

--- .travis_script.bash
+++ .travis_script.bash
@@ -16,7 +16,7 @@ verify-changelog(){
   pushd /tmp
 
   git clone "https://github.com/ponylang/changelog-tool"
-  cd changelog-tool && git checkout tags/0.1.4 && make && sudo make install && cd -
+  cd changelog-tool && git checkout tags/0.2.0 && make && sudo make install && cd -
 
   popd
 
