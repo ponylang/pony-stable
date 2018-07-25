d73010944f003d90e5c27356256c864d073df961
Dipin Hora <dipin@wallaroolabs.com>
2018-07-25 01:29:16 +0000

    Make sure libpcre2-dev gets installed by apt

--- .travis_install.bash
+++ .travis_install.bash
@@ -7,7 +7,9 @@ install_ponyc(){
   echo -e "\033[0;32mInstalling latest ponyc release\033[0m"
   sudo add-apt-repository ppa:ponylang/ponylang -y
   sudo apt-get update
-  sudo apt-get -V install ponyc -y
+  # NOTE: libpcre2-dev is specified because otherwise
+  # apt refuses to install it and everything fails
+  sudo apt-get -V install ponyc libpcre2-dev -y
 }
 
 install_ponyc
